29126a0f85e91c304203a18558e05415e54d285d
kutselabskii <osutanfirst@gmail.com>
2019-01-31 16:38:50 +1000

    Add monogame project template

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MatchThree
+{
+#if WINDOWS || LINUX
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MatchThreeGame())
+                game.Run();
+        }
+    }
+#endif
+}
